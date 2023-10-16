df6a40bfc020a64f232d4a8af7e34c54fd4a5124
MarcosDarpa <130925272+MarcosDarpa@users.noreply.github.com>
2023-10-15 22:58:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 function tocaSom(idElementoAudio) {
   document.querySelector(idElementoAudio).play();
 }
-
 const listaDeTeclas = document.querySelectorAll(".tecla");
-
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
-
+for (const tecla of listaDeTeclas) {
+  const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
-  console.log(idAudio);  
-  listaDeTeclas[contador].onclick = function () {
-    tocaSom('#tecla_');
-  }
-
-  contador = contador + 1;
-
-  console.log(contador);
+  console.log(idAudio);
+  tecla.addEventListener("click", function () {
+    tocaSom(idAudio);
+  });
 }
