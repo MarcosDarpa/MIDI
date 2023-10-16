7401be2b7a360cbffbffa5a2b5051dfeecd55647
MarcosDarpa <130925272+MarcosDarpa@users.noreply.github.com>
2023-10-16 11:37:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+  const elemento = document.querySelector(idElementoAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
 }
+
 const listaDeTeclas = document.querySelectorAll(".tecla");
+
 for (const tecla of listaDeTeclas) {
   const instrumento = tecla.classList[1];
+
   const idAudio = `#som_${instrumento}`;
-  console.log(idAudio);
   tecla.addEventListener("click", function () {
     tocaSom(idAudio);
+  });
+
+  tecla.addEventListener("keydown", function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add('ativa')
+    }
+  });
+
+  tecla.addEventListener("keyup", function () {
+    tecla.classList.remove("ativa");
   });
 }
